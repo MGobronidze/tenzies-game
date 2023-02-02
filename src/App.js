f429659b523a4b683cc643ef7ac46674c2e08827
MGobronidze <90101429+MGobronidze@users.noreply.github.com>
2023-02-02 10:34:42 +0400

    Adding Dies and buttons

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Die from './components/Die';
+import {nanoid} from "nanoid"
+
 
 function App() {
+  const [dice , setDice] = React.useState(allNewDice());
+
+  function allNewDice(){
+    let newDice=[]
+    for (let i=0; i<10; i++){
+          newDice.push(
+            {
+            value: Math.trunc(Math.random()*6 + 1),
+            isHeld: false,
+            id: nanoid()
+            }
+          )
+    }
+    return newDice
+  }
+
+  function rollDice(){
+      setDice(allNewDice());
+  }
+
+  function holdDice(id) {
+    setDice(oldDice => oldDice.map(die => {
+        return die.id === id ? 
+            {...die, isHeld: !die.isHeld} :
+            die
+    }))
+}
+  const DiceEl = dice.map( die => <Die key={die.id} value ={die.value} isHeld={die.isHeld} holdDice={() => holdDice(die.id)} />)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <main> 
+      <div className='dice-container '>
+        {DiceEl} 
+      </div> 
+      <button className="roll-btn" onClick={rollDice}>Roll</button>
+  </main>
   );
 }
 
